b92ac0d51463a66d507ddd8d17e659cf4d9d9c27
Biuuu2309 <minhvu7a1@gmail.com>
2024-09-22 08:59:00 +0700

    Update Response.cs

--- WindowsForm_Project/WindowsForm_Project/Models/Response.cs
+++ WindowsForm_Project/WindowsForm_Project/Models/Response.cs
@@ -11,7 +11,7 @@ namespace WindowsForm_Project.Models
         public string statusmessage { get; set; }
         public List<Room> list { get; set; }
         public List<Customer> list1 { get; set; }
-        public List<Employee> list2 { get; set; }
+        public List<Manage> list2 { get; set; }
         public List<Account> list3 { get; set; }
     }
 }
