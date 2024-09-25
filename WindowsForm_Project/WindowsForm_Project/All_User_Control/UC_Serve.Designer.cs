4a80cdf258cf1f36b5a10b40ecdbee12941e8aee
Biuuu2309 <minhvu7a1@gmail.com>
2024-09-25 13:02:02 +0700

    aaaa

---
+++ WindowsForm_Project/WindowsForm_Project/All_User_Control/UC_Serve.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace WindowsForm_Project.All_User_Control
+{
+    partial class UC_Serve
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // UC_Serve
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "UC_Serve";
+            this.Size = new System.Drawing.Size(1920, 966);
+            this.Load += new System.EventHandler(this.UC_Serve_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
